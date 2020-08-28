a7f8c131b3c516f6dcbb2158adca38debea42043
rajarahul12 <maddula.rajarahul@gmail.com>
2020-08-28 23:25:37 +0530

    Adding list detail screen and other components

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ListingDetailsScreen />;
 }
