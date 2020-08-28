3dee66eeb57a4a76f8f6a1674095e955f32b5f64
rajarahul12 <maddula.rajarahul@gmail.com>
2020-08-28 23:39:34 +0530

    styling changes

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Image, View } from "react-native";
 import AppText from "../components/AppText";
 import colors from "../config/colors";
+import ListItem from "../components/ListItem";
 
 export default function ListingDetailsScreen() {
   return (
@@ -10,6 +11,13 @@ export default function ListingDetailsScreen() {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red Jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title="Mosh Hambridge"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -32,5 +40,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
