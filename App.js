d1e8d6dd20597c0c5fab075b39b873c502e3f2c3
rajarahul12 <maddula.rajarahul@gmail.com>
2020-08-29 23:24:01 +0530

    Building text input

--- App.js
+++ App.js
@@ -4,11 +4,17 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import AppTextInput from "./app/components/AppTextInput";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
