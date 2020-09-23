b330ce21ce1838c5d17b4860cbbb4921550f2601
rajarahul12 <maddula.rajarahul@gmail.com>
2020-09-24 00:28:52 +0530

    Adding Image picker and location access

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import LoginScreen from "./app/screens/LoginScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
